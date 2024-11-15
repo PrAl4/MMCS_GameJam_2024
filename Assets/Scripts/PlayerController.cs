24e716ce4fa60e1d74c6cbece9f18bd9fc799c3f
Vladislav Melnikov <melnikoff2222@gmail.com>
2024-11-15 15:25:23 +0300

    mod2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,19 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using System.Linq;
 
 public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] float speed = 3f;
+    [SerializeField] private float jumpForce = 3000000000f;
     private Rigidbody2D rb;
-    private Vector2 move;
+    private float move;
+    private bool isGrounded;
+    private SpriteRenderer spriteRenderer;
     //private Animator animator;
-    private Vector2 zero = new Vector2(0, 0);
+
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rb.inertia = 0;
     }
 
 
@@ -25,19 +31,62 @@ public class PlayerController : MonoBehaviour
         Cursor.visible = false;
     }
 
-
     void Update()
     {
-        //animator.SetBool("IsRuning", false);
-        move = new Vector2(Input.GetAxis("Horizontal"), 0);
-        if (move != zero)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            //animator.SetBool("IsRuning", true);
-            Vector2 velocity = new Vector2(move[0] * speed, rb.velocity.y);
-
-            rb.velocity = velocity;
+            isGrounded = false;
+            Jump();
         }
+    }
+
+    void FixedUpdate()
+    {
+
+        float inputAxis = Input.GetAxis("Horizontal");
+        Vector2 velocity = rb.velocity;
+        velocity.x = inputAxis * speed;
+        rb.velocity = velocity;
         
+
+        if (inputAxis > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (inputAxis < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        if (isGrounded)
+        {
+            if (inputAxis != 0)
+            {
+                //animator.SetBool("IsRuning", true);
+            }
+            else
+            {
+                //animator.SetBool("IsRuning", false);
+            }
+        }
+
+    }
+    void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!isGrounded)
+        {
+            isGrounded = collision.contacts.All(c => c.point.y < transform.position.y);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+
+        isGrounded = collision.contacts.All(c => c.point.y > transform.position.y);
 
     }
 }
