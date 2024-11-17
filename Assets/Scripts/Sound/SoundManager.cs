d6948f1759fc9cb519db750653003ec86412a548
PumPururururum <obrigankenobi66@gmail.com>
2024-11-17 11:26:19 +0300

    changing footstep sound

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -24,10 +24,6 @@ public class SoundManager : MonoBehaviour
         {
             FootstepsCoroutine();
         }
-        else
-        {
-            //StopAllCoroutines();
-        }
     }
 
     // Update is called once per frame
