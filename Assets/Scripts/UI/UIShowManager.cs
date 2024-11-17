c0a99c06ba4584fb6b91964b9124bdf7063fe3e1
PumPururururum <obrigankenobi66@gmail.com>
2024-11-17 16:44:16 +0300

    applied some sounds

--- Assets/Scripts/UI/UIShowManager.cs
+++ Assets/Scripts/UI/UIShowManager.cs
@@ -9,17 +9,25 @@ public class UIShowManager : MonoBehaviour
 
     public static bool wheelIsActive = false;
 
+    private SoundManager soundManager;
+    private void Start()
+    {
+        soundManager = GameObject.FindGameObjectWithTag("SoundManager").GetComponent<SoundManager>();
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.R) && !wheelIsActive)
         {
             WheelOfChoice.SetActive(true);
             wheelIsActive = true;
+            soundManager.PlayButtonSound();
         }
         else if (Input.GetKeyDown(KeyCode.R) && wheelIsActive) 
         {
             WheelOfChoice.SetActive(false);
             wheelIsActive = false;
+            soundManager.PlayButtonSound();
         }
     }
 }
